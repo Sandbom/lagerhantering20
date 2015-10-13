ce5f48ebadb0a68c2be116d90472f7b1cfc9643d
Eric Falheim <ericfalheim@hotmail.com>
2015-10-13 20:34:51 +0200

    fungerande lagerio.h
    Behövs kommenteras

--- lagerio.h
+++ lagerio.h
@@ -1,46 +1,19 @@
-/// Valideringsfunktinoen tar emot en pekare till
-/// data och kontrollerar om datat kan omvandlas till avsedd typ.
-/// Exempel:
-/// - valid_int
-/// - valid_shelf
-typedef bool (*v_f) (char *);
+#include <stdlib.h>
+#include <stdbool.h>
 
-/// Konstruktor som tar emot en sträng med validerat data
-/// och omvandlar det till avsedd typ, returnerar en pekare
-/// till det nya datat.
-/// Exempel:
-/// str_to_int
-/// str_to_shelf
-typedef *(*m_f)(char *);
+typedef bool (*v_f)(char *);   // validate_function
+typedef void *(*m_f)(char *);  // modify_function
 
 typedef struct shelf shelf_t;
 
-// Prints q and return answer as string.
-char *ask_question_string(char *q);
-
-//Prints q and return answer as integer.
-int ask_int_question(char *q);
-
-//
-int read_int(bool rep);
-
-//
-bool is_valid_int(char *str);
-
-//
-char *read_string(bool s_n);
-
-//
-char *read_string_with_buffer(char *buf, size_t len, bool s_n);
-
-//
-shelf_t ask_shelf_question(void);
-
-//
-void *ask_question(char *q, v_f v, m_f co, bool cl);
-
-//
-bool valid_shelf(char *input);
-
-
-
+char *read_string(void);
+char *get_string_with_buffer(char *b, size_t l, bool s_n);
+bool is_valid_int(char *s);
+bool is_valid_shelf(char *s);
+char *str_to_str(char *s);
+int str_to_int(char *s);
+shelf_t *str_to_shelf(char *s); 
+void *ask_question(char *q, v_f v, m_f m, bool c);
+char *ask_string_question(void);
+int *ask_int_question(void);
+shelf_t *ask_shelf_question(void);
